d402703575c7b54c4771e247402e336aa2e43b07
qittlab <nams@nist.gov>
2020-12-02 09:58:02 -0700

    add table svelte stuff

---
+++ src/ChTable.svelte
@@ -0,0 +1,37 @@
+<script>
+	import InlineInput2 from './input.svelte';
+
+	let extras = {min: -100, max:100, style:"width: 5em;"}
+	let min = 1
+	let columns = ['Name', 'value', 'on/off']
+	export let data = [];
+// 	for (let i=0; i<3; i++) {
+// 		data.push(['ch'+i, i+1, false])
+// 	}
+
+</script>
+<style>
+	tr td {
+		text-align: center; 
+		vertical-align: middle;
+		width: 5em;
+		height: 2em;
+		background: #eee;
+	}
+</style>
+
+<table>	
+	<tr>
+		{#each columns as column}
+		<th>{column}</th>
+		{/each}
+	</tr>
+	{#each data as row}
+	<tr>
+		<td><InlineInput2 bind:value={row[0]} extras={{style:"width: 5em;"}} /></td>
+		<td><InlineInput2 type='number' bind:value={row[1]} extras={extras}/></td>
+		<td style=""><input type=checkbox bind:checked={row[2]}/></td>
+	</tr>
+	{/each}
+</table>
+
---
+++ src/input.svelte
@@ -0,0 +1,124 @@
+<script>
+import { tick, createEventDispatcher } from 'svelte';
+
+const dispatch = createEventDispatcher();
+
+// Props
+export let value = '';
+export let type = 'text';
+export let placeholder = '';
+export let labelClasses = '';
+export let inputClasses = '';
+export let rows = 2;
+export let cols = 20;
+export let options = [];
+export let extras={};
+
+let editing = false;
+let inputEl;
+let label;
+let selectedIndex = options.findIndex(o => o.value === value);
+let e;
+	
+// Computed
+$: isText = type === 'text';
+$: isNumber = type === 'number';
+$: isTextArea = type === 'textarea';
+$: isSelect = type === 'select';
+$: if (isNumber) {
+      label = value === '' ? placeholder : value;
+    } else if (isText || isTextArea) {
+      label = value ? value : placeholder;
+    } else { // Select
+      label = selectedIndex === -1 ? placeholder : options[selectedIndex].label;
+    }
+
+const toggle = async (_) => {
+  editing = !editing;
+
+  if (editing) {
+    await tick();
+    inputEl.focus();
+  }
+};
+
+const handleInput = (e) => {
+  value = isNumber ? +e.target.value : e.target.value;
+};
+
+const handleEnter = (e) => {
+  if (e.keyCode === 13) inputEl.blur();
+};
+
+const handleBlur = (e) => {
+  toggle();
+	console.log('blur', e)
+	if (e.srcElement.max!="") {
+		let max = Number(e.srcElement.max)
+		if (value>max) value = max;
+	}
+	if (e.srcElement.min!="") {
+		let min = Number(e.srcElement.min)
+		if (value<min) value = min;
+	}
+  dispatch('blur', value);
+	
+};
+
+const handleChange = (e) => {
+  selectedIndex = placeholder ? e.target.selectedIndex - 1 : e.target.selectedIndex;
+  value = options[selectedIndex].value;
+};
+</script>
+
+{#if editing && (isText || isNumber)}
+  <input
+    class={inputClasses}
+    bind:this={inputEl}
+    {type}
+    {value}
+    {placeholder}
+		{...extras}
+    on:input={handleInput}
+    on:keyup={handleEnter}
+    on:blur={handleBlur}>
+{:else if editing && isTextArea}
+  <textarea
+    class={inputClasses}
+    bind:this={inputEl}
+    {placeholder}
+    {value}
+    {rows}
+    {cols}
+    on:input={handleInput}
+    on:blur={handleBlur} />
+{:else if editing && isSelect}
+  <select
+    class={inputClasses}
+    bind:this={inputEl}
+    on:change={handleChange}
+    {value}
+    on:blur={handleBlur}>
+    {#if placeholder}
+      <option selected value disabled>{placeholder}</option>
+    {/if}
+    {#each options as { label, value }, i}
+      <option
+        key={i}
+        {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+{:else}
+  <span
+    class={labelClasses}
+    on:click={toggle}>
+    {label}
+    <slot name="selectCaret">
+      {#if isSelect}
+        <span>&#9660;</span>
+      {/if}
+    </slot>
+  </span>
+{/if}
